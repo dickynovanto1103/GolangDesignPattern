64e754fa5305ca2612f4453ef0cfa3b5a690d482
Dicky Novanto <dickynovanto1103@gmail.com>
2020-02-08 17:44:28 +0800

    add singleton design pattern

---
+++ creational/singleton/instance.go
@@ -0,0 +1,29 @@
+package main
+
+import (
+	"fmt"
+	"sync"
+)
+
+type instance struct{}
+
+var singleIns *instance
+
+var lock = &sync.Mutex{}
+
+func getInstance() *instance {
+	if singleIns == nil {
+		lock.Lock()
+		defer lock.Unlock()
+		if singleIns == nil {
+			fmt.Println("creating object")
+			singleIns = &instance{}
+			return singleIns
+		}
+		fmt.Println("singleIns has been created after acquiring lock")
+	} else {
+		fmt.Println("singleIns has been created before")
+	}
+
+	return singleIns
+}
