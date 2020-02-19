0b1dfb8c01693e85e99e2c77f96efd0a90755ef2
Dicky Novanto <dickynovanto1103@gmail.com>
2020-02-20 00:17:42 +0800

    add view main code

--- behavioural/state/main.go
+++ behavioural/state/main.go
@@ -3,4 +3,6 @@ package main
 func main() {
 	vendingMachine := NewVendingMachine(1, 1)
 	vendingMachine.CurrentState.RequestItem()
+	vendingMachine.CurrentState.DispenseItem()
+	vendingMachine.CurrentState.RequestItem()
 }
